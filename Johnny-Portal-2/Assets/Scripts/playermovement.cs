fc259f2ed73047b3deae3cbbe9279211f6d945e2
JohnnySkateboard <114027165+JohnnySkateboard@users.noreply.github.com>
2022-10-11 14:06:12 -0700

    Big Changes To Movement

--- Johnny-Portal-2/Assets/Scripts/playermovement.cs
+++ Johnny-Portal-2/Assets/Scripts/playermovement.cs
@@ -1,38 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class playermovement : MonoBehaviour
 {
-    // references the movement manager script, making this script
-    // significantly easier to write
-    public MovementManager controller;
+	private Vector3 targetVelocity;
+	private float move;
+	public Rigidbody2D rb;
+	public float MovementSmoothing;
+	private Vector3 Velocity = Vector3.zero;
+	public bool isgrounded;
+	public bool israiled;
+	public Collider2D coll;
 
-    public float runspeed = 40f;
-    float Horizontalmove = 0f;
-
-    bool jump = false;
-    
-    // Update is called once per frame
-    void Update()
+	void Update()
     {
-        // left + right / a + d will move the player horizontal
-        Horizontalmove = Input.GetAxisRaw("Horizontal") * runspeed;
+		move = 0;
 
-        // Jump is false by default, but turns on when space is pressed
-        if (Input.GetButtonDown("Jump"))
+		if (Input.GetKey("a"))
+		{
+			move = -1;
+		}
+
+		if (Input.GetKey("d"))
+		{
+			move = 1;
+		}
+
+		if (isgrounded == true)
+		{
+			if (Input.GetKeyDown("space"))
+			{
+				rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+			}
+		}
+
+		if(coll.IsTouchingLayers(LayerMask.GetMask("Rail")))
         {
-            jump = true;
+			israiled = true;
+
+            if(Input.GetKey("x"))
+            {
+				GameObject[] rail = GameObject.FindGameObjectsWithTag("Rail");
+				foreach (GameObject r in rail)
+					r.GetComponent<BoxCollider2D>().isTrigger = false;
+			}
+			if (!Input.GetKey("x"))
+			{
+				GameObject[] rail = GameObject.FindGameObjectsWithTag("Rail");
+				foreach (GameObject r in rail)
+					r.GetComponent<BoxCollider2D>().isTrigger = true;
+
+			}
+
+		}
+
+		if(!coll.IsTouchingLayers(LayerMask.GetMask("Rail")))
+        {
+			israiled = false;
         }
 
+		targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref Velocity, MovementSmoothing);
+	}
 
-    }
+	void OnCollisionEnter2D(Collision2D theCollision)
+	{
+		if (theCollision.gameObject.name == "Ground")
+		{
+			isgrounded = true;
+		}
+	}
 
-    void FixedUpdate ()
-    {
-        // Move the Thing, false says crouching is a no go, sets just to false after the player jumps 
-        // so that a loop is not created.
-        controller.Move(Horizontalmove * Time.fixedDeltaTime, false, jump);
-        jump = false;
-    }
+	void OnCollisionExit2D(Collision2D theCollision)
+	{
+		if (theCollision.gameObject.name == "Ground")
+		{
+			isgrounded = false;
+		}
+	}
 }
